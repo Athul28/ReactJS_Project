9927fa847eca9f4fdc82db6bcc27880486b0956b
Athul Bhandary <athulbhandary28@gmail.com>
2025-04-14 19:19:11 +0530

    fix: build errors

--- src/app/reference/page.tsx
+++ src/app/reference/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-import { LatestPost } from "~/app/_components/post";
+// import { LatestPost } from "~/app/_components/post";
 import { auth } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
 import GeminiChat from "../_components/GeminiChat";
@@ -9,9 +9,9 @@ export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
   const session = await auth();
 
-  if (session?.user) {
-    void api.post.getLatest.prefetch();
-  }
+  // if (session?.user) {
+  //   void api.post.getLatest.prefetch();
+  // }
 
   return (
     <HydrateClient>
@@ -62,7 +62,7 @@ export default async function Home() {
             </div>
           </div>
 
-          {session?.user && <LatestPost />}
+          {/* {session?.user && <LatestPost />} */}
           {session?.user && <GeminiChat />}
         </div>
       </main>
